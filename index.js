66a4b4bd7a2dc8b62eafbc9c296b283b238edc91
Samarth Trivedi <138616723+samarth06trivedi@users.noreply.github.com>
2024-02-02 23:36:22 +0530

    index.js

--- index.js
+++ index.js
@@ -3,39 +3,39 @@ let p2 = Math.floor(Math.random()*5 + 1);
 
 /* playr 1 */
 if(p1==2){
-    document.querySelector(".dice1").setAttribute("src","dice2.png");
+    document.querySelector(".dice1").setAttribute("src","./dice2.png");
 }
 if(p1==3){
-    document.querySelector(".dice1").setAttribute("src","dice3.png");
+    document.querySelector(".dice1").setAttribute("src","./dice3.png");
 }
 
 if(p1==4){
-    document.querySelector(".dice1").setAttribute("src","dice4.png");
+    document.querySelector(".dice1").setAttribute("src","./dice4.png");
 }
 if(p1==5){
-    document.querySelector(".dice1").setAttribute("src","dice5.png");
+    document.querySelector(".dice1").setAttribute("src","./dice5.png");
 }
 if(p1==6){
-    document.querySelector(".dice1").setAttribute("src","dice6.png");
+    document.querySelector(".dice1").setAttribute("src","./dice6.png");
 }
 
 /* player 2 */
 
 if(p2==2){
-    document.querySelector(".dice2").setAttribute("src","dice2.png");
+    document.querySelector(".dice2").setAttribute("src","./dice2.png");
 }
 if(p2==3){
-    document.querySelector(".dice2").setAttribute("src","dice3.png");
+    document.querySelector(".dice2").setAttribute("src","./dice3.png");
 }
 
 if(p2==4){
-    document.querySelector(".dice2").setAttribute("src","dice4.png");
+    document.querySelector(".dice2").setAttribute("src","./dice4.png");
 }
 if(p2==5){
-    document.querySelector(".dice2").setAttribute("src","dice5.png");
+    document.querySelector(".dice2").setAttribute("src","./dice5.png");
 }
 if(p2==6){
-    document.querySelector(".dice2").setAttribute("src","dice6.png");
+    document.querySelector(".dice2").setAttribute("src","./dice6.png");
 }
 
 /* title */
